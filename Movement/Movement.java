fb79fd9813c8ae185a2e1286dc5242486ddbacdb
kevealci <kevealci@espol.edu.ec>
2021-11-25 09:56:20 -0500

    En el paquete movement se encarga de gestionar las funcionalidades del sistema

--- Movement/Movement.java
+++ Movement/Movement.java
@@ -1,4 +1,35 @@
 package Movement;
 
+import Transaction.*;
+import Wrappers.*;
+
 public class Movement {
+  private Money balance;
+  private Transaction transaction;
+
+  public Movement(Money balance, Deposit deposit) {
+    this.balance = balance;
+    this.transaction = deposit;
+  }
+
+  public Movement(Money balance, Withdraw withdraw) {
+    this.balance = balance;
+    this.transaction = withdraw;
+  }
+
+  public Movement(Money balance, Transfer transfer) {
+    this.balance = balance;
+    this.transaction = transfer;
+  }
+
+  public boolean isDeposit() {return transaction instanceof Deposit;}
+
+  public boolean isWithDraw() {return transaction instanceof Withdraw;}
+
+  public void DateFilter() {System.out.println(transaction.getDate() + "                                   " + balance);}
+
+  @Override
+  public String toString() {
+    return transaction + "   " + balance;
+  }
 }
--- Movement/MovementList.java
+++ Movement/MovementList.java
@@ -1,4 +1,39 @@
 package Movement;
 
+import java.util.ArrayList;
+
 public class MovementList {
+
+  private ArrayList<Movement> movements;
+
+  public MovementList() {this.movements = new ArrayList<Movement>();}
+
+  public void addMovement(Movement movement) {movements.add(movement);}
+
+  public ArrayList<Movement> depositFilter() {
+    ArrayList<Movement> deposits = new ArrayList<Movement>();
+    movements.forEach(movement -> { if(movement.isDeposit()) deposits.add(movement);});
+    return deposits;
+  }
+
+  private ArrayList<Movement> withDrawFilter() {
+    ArrayList<Movement> withDraws = new ArrayList<Movement>();
+    movements.forEach(movement -> {if(movement.isWithDraw()) withDraws.add(movement);});
+    return withDraws;
+  }
+
+  public void dateFilter() {printHeader();movements.forEach(Movement::DateFilter);}
+
+  private void printFilter(ArrayList<Movement> movements) {movements.forEach(System.out::println);}
+
+  public void printWithDraws() {printHeader();printFilter(withDrawFilter());}
+
+  public void printDeposits() {printHeader();printFilter(depositFilter());}
+
+  public void printMovements() {printHeader();printFilter(movements);}
+
+  public void printHeader() {
+    System.out.println("----------------Movements----------------");
+    System.out.println("   Date     Deposit   WithDraw   Transfer   Balance");
+  }
 }
