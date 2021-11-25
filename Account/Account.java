de801dd6801de671e16b2caafdedb840c88227db
kevealci <kevealci@espol.edu.ec>
2021-11-25 09:54:44 -0500

    El paquete Account tiene las clases que se encargan de manejar el sistema

--- Account/Account.java
+++ Account/Account.java
@@ -1,4 +1,27 @@
 package Account;
 
+import Wrappers.*;
+
 public class Account {
+  private Statement statement;
+
+  public Account(Statement statement){
+    this.statement = statement;
+  }
+
+  public void deposit(Money amount, Date date) {statement.deposit(amount,date);}
+
+  public void withDraw(Money amount, Date date) {statement.withDraw(amount,date);}
+
+  public void sendTransfer(Account account, Money amount, Date date) {statement.sendTransfer(account,amount,date);}
+
+  public void receiveTransfer(Money amount, Date date) {statement.receiveTransfer(amount,date);}
+
+  public void printStatements() {statement.printStatements();}
+
+  public void printDeposits() {statement.printDeposits();}
+
+  public void printWithDraws() {statement.printWithDraws();}
+
+  public void printDatesFilter() {statement.printDatesFilter();}
 }
--- Account/Statement.java
+++ Account/Statement.java
@@ -1,4 +1,49 @@
 package Account;
 
+import Movement.*;
+import Transaction.*;
+import Wrappers.*;
+
 public class Statement {
+  private Money globalBalance;
+  private MovementList movementList;
+
+  public Statement() {
+    this.globalBalance = new Money(0);
+    this.movementList = new MovementList();
+  }
+
+  public void deposit(Money amount, Date date){
+    this.globalBalance = sumAmount(globalBalance, amount);
+    movementList.addMovement( new Movement( globalBalance, new Deposit(amount,date) ) );
+  }
+
+  public void withDraw(Money amount, Date date) {
+    globalBalance = minusAmount(globalBalance,amount);
+    movementList.addMovement( new Movement( globalBalance, new Withdraw(amount,date) ) );
+
+  }
+
+  public void sendTransfer(Account account,Money amount, Date date) {
+    globalBalance = minusAmount(globalBalance,amount);
+    movementList.addMovement( new Movement( globalBalance, new Transfer(amount,date)));
+    account.receiveTransfer(amount,date);
+  }
+
+  public void receiveTransfer(Money amount, Date date) {
+    globalBalance = sumAmount(globalBalance,amount);
+    movementList.addMovement( new Movement( globalBalance, new Transfer(amount,date)));
+  }
+
+  public Money sumAmount(Money amountA, Money amountB) {return new Money(amountA.getAmount()+amountB.getAmount());}
+
+  public Money minusAmount(Money amountA, Money amountB) {return new Money(amountA.getAmount()-amountB.getAmount());}
+
+  public void printStatements() {movementList.printMovements();}
+
+  public void printDeposits() {movementList.printDeposits();}
+
+  public void printWithDraws() {movementList.printWithDraws();}
+
+  public void printDatesFilter() {movementList.dateFilter();}
 }
