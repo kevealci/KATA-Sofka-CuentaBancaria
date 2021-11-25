aa7b49429189f89cf7067a07096fcefc2f3f254f
kevealci <kevealci@espol.edu.ec>
2021-11-25 09:57:56 -0500

    Se encapsulo las variables primitivas en clases para tener un mejor control de los datos

--- Wrappers/Date.java
+++ Wrappers/Date.java
@@ -1,4 +1,23 @@
-package Wrapers;
+package Wrappers;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Date {
+    private LocalDate date;
+
+    public Date(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.date = LocalDate.parse(date,formatter);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(formatter);
+    }
 }
--- Wrappers/Money.java
+++ Wrappers/Money.java
@@ -1,4 +1,16 @@
-package Wrapers;
+package Wrappers;
 
 public class Money {
+    private double amount;
+
+    public Money(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {return String.valueOf(amount);}
 }
